1c032b5e71dd5747a932732f933e7574c0694bb4
“MadhavEY” <madhav.varshney@in.ey.com>
2024-08-20 13:53:32 +0530

    registration form changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
-  {path:'login',component:LoginComponent},
+  // {path:'login',component:LoginComponent},
   {path:'signup', component:SignupComponent},
   {path:'dashboard',component:DashboardComponent},
   {path:'register', component: RegistrationComponent},
