4fe21d7e955997cf1a11a84d5ee32f95f9658d00
Adarsh Verma <Adarsh.Verma1@in.ey.com>
2024-08-20 11:52:01 +0530

    changes for cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { InsurTableComponent } from './components/insur-table/insur-table.component';
+import { ContainerComponent } from './components/container/container.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
   // {path:'login',component:LoginComponent},
   {path:'dashboard',component:DashboardComponent},
   {path:'register', component: RegistrationComponent},
-  {path: 'insur-table', component: InsurTableComponent}
+  {path: 'insur-table', component: InsurTableComponent},
+  {path:'grid',component:ContainerComponent},
 ];
 
 @NgModule({
--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { MotifIconModule,MotifCardModule } from '@ey-xd/ng-motif';
-import {MotifMapsIcDirectionsCar24px,MotifMapsIcFlight24px,MotifMapsIcDirectionsBike24px} from '@ey-xd/motif-icon';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [MotifIconModule,MotifCardModule],
+  imports: [],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
 export class ContainerComponent {
-  myIconFunction(){
-    return MotifMapsIcDirectionsCar24px;
 
-  }
 }
