10f31a8265e0a8d0cba5fa3252f5d20efdd639ae
Sanal Jain <sanal.jain@in.ey.com>
2024-08-29 15:41:07 +0530

    product form changes

--- src/app/components/product/create-product/create-product.component.ts
+++ src/app/components/product/create-product/create-product.component.ts
@@ -4,16 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { ButtonSizes, MotifContentSwitcherModule } from '@ey-xd/ng-motif';
 import { ProductDetailsComponent } from '../product-details/product-details.component';
 import { ProductInfoFormComponent } from '../product-info-form/product-info-form.component';
+import { CoverageInfoComponent } from '../coverage-info/coverage-info.component';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [MotifContentSwitcherModule,FormsModule,CommonModule, ProductDetailsComponent,ProductInfoFormComponent],
+  imports: [MotifContentSwitcherModule,FormsModule,CommonModule, ProductDetailsComponent,ProductInfoFormComponent,CoverageInfoComponent],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent implements OnInit {
-  Options = [{ value: 'Product Details', selected: true }, { value: 'Product Info', selected: false }, { value: 'Coverage & Info', selected: false },{ value: 'Rider & Info', selected: false },{ value: 'Documents', selected: false }];
+  Options = [{ value: 'Product Details', selected: true }, { value: 'Product Info', selected: false }, { value: 'Coverage & Info', selected: false }];
  
   constructor(){}
 
