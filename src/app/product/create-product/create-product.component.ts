18b94ac885b1a308669de36cc5b96ef32d6c787f
Sanal Jain <sanal.jain@in.ey.com>
2024-08-29 11:20:05 +0530

    product changes

--- src/app/product/create-product/create-product.component.ts
+++ src/app/product/create-product/create-product.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonSizes, MotifContentSwitcherModule } from '@ey-xd/ng-motif';
+import { ProductDetailsComponent } from 'src/app/components/product-details/product-details.component';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [MotifContentSwitcherModule,FormsModule,ProductDetailsComponent,CommonModule],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
-export class CreateProductComponent {
+export class CreateProductComponent implements OnInit {
+  Options = [{ value: 'Product Details', selected: true }, { value: 'Product Info', selected: false }, { value: 'Coverage & Info', selected: false }];
+ 
+  constructor(){}
 
+  ngOnInit(): void {
+    
+  }
+
+  isFormDisabled(value : string){
+    
+    return false;
+  }
+
+  
 }
